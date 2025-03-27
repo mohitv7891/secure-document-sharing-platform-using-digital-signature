8533dec984059fe4360c9bcab3f4590af9a6adf5
mohitv7891 <mohitvishwakarma7891@gmail.com>
2025-03-27 18:24:21 +0530

    modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
       <h1 className="text-3xl font-bold">Secure Document Sharing Platform</h1>
+      <h2>Arun</h2>
     </div>
   );
 }
