066508ad011681df298ee1359622a172bfab689a
mohitv7891 <mohitvishwakarma7891@gmail.com>
2025-03-27 18:20:01 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">Secure Document Sharing Platform</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
